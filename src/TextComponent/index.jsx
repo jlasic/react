7fa6d27fcc43be2cf8038a10deece2fe0fa65f12
Josip Lasic <lasic.josip96@gmail.com>
2018-05-27 11:02:57 +0200

    fetch random color

--- src/TextComponent/index.jsx
+++ src/TextComponent/index.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class TextComponent extends Component {
+    constructor(props){
+        super(props);
+        this.defaultColor = '000000';
+        this.randomColor = '';
+        this.state = {
+            toggle: false
+        };
+
+        const self = this;
+        fetch('http://www.colr.org/json/color/random')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            self.randomColor = json.colors[0].hex;
+            self.setState({
+                toggle: true
+            });
+        });
+    }
     render() {
       return (
-        <p>
-            Hello World!
+        <p >
+            {this.randomColor}
         </p>
       );
     }
