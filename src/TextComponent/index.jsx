219dacdcb80fb29ab8e2f6ed1842760ae2d7e6d4
Josip Lasic <lasic.josip96@gmail.com>
2018-05-27 10:34:17 +0200

    text component created

---
+++ src/TextComponent/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TextComponent extends Component {
+    render() {
+      return (
+        <p>
+            Hello World!
+        </p>
+      );
+    }
+  }
+  
+export default TextComponent;
